c3a0e1197849608f5ba9b08f4d2125d2887db4a9
shafrasier <118404584+shafrasier@users.noreply.github.com>
2024-01-18 16:18:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,41 @@
-console.log("Script loaded");
-
 document.addEventListener('DOMContentLoaded', function() {
-    console.log("DOM fully loaded and parsed");
+  // Remove any exit animations when the DOM is fully loaded
+  document.body.classList.remove('back-button-exit-active', 'home-page-exit-active');
 
   // Event listener for the back button
   var backButton = document.querySelector('.back-button');
   if (backButton) {
-    console.log("Back button found");
     backButton.addEventListener('click', function() {
-      console.log("Back button clicked");
       if (window.scrollY === 0) {
-        console.log("At top of the page, adding exit animation class");
         document.body.classList.add('back-button-exit-active');
-
         // Wait for the animation to finish before redirecting
         setTimeout(function() {
-          console.log("Redirecting to home page");
           window.location.href = 'index.html'; // Replace with the home page URL
         }, 500); // This should match the duration of the animation
       } else {
-        console.log("Not at top of the page, scrolling to top");
         window.scrollTo({ top: 0, behavior: 'smooth' });
       }
     });
-  } else {
-    console.log("Back button not found");
   }
 
   // Event listeners for other buttons
   document.querySelectorAll('.button').forEach(button => {
-    console.log("Setting up click event listener for button");
     button.addEventListener('click', function() {
       var destinationURL = this.getAttribute('data-destination-url');
-      console.log("Button clicked, destination URL: " + destinationURL);
       if (destinationURL) {
-        document.body.classList.add('home-page-exit-active'); // Add exit animation
-        console.log("Exit animation class added, waiting to redirect");
-
+        document.body.classList.add('home-page-exit-active');
         // Redirect after the animation
         setTimeout(function() {
-          console.log("Redirecting to: " + destinationURL);
           window.location.href = destinationURL;
         }, 500); // Ensure this matches the animation duration
       }
     });
   });
+});
+
+// Handle the pageshow event for BFCache pages
+window.addEventListener('pageshow', function(event) {
+  if (event.persisted) {
+    document.body.classList.remove('back-button-exit-active', 'home-page-exit-active');
+  }
 });
